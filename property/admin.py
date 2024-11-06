fd4e8406a32a4f59cd4dacf8fc35e4f3f6c7d354
DanilaKorsakov <109290153+DanilaKorsakov@users.noreply.github.com>
2024-11-06 15:34:17 +0300

    setup admin UI for likes

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     list_editable = ['new_building']
+    raw_id_fields=['liked_by']
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('author', 'flat')
--- property/models.py
+++ property/models.py
@@ -49,13 +49,30 @@ class Flat(models.Model):
 
     new_building = models.BooleanField(blank=True,  null=True)
 
+    liked_by = models.ManyToManyField(
+        User,
+        blank=True,
+        related_name='liked_flats',
+        verbose_name='Кто лайкнул'
+    )
+
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
 
 
 class Complaint(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Кто жаловался', related_name='complaints')
-    flat = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name='Квартира, на которую пожаловались', related_name='complaints')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Кто жаловался',
+        related_name='complaints'
+    )
+    flat = models.ForeignKey(
+        Flat,
+        on_delete=models.CASCADE,
+        verbose_name='Квартира, на которую пожаловались',
+        related_name='complaints'
+    )
     complaint_text = models.TextField('Текст жалобы')
 
     def __str__(self):
