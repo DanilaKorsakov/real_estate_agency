08f8d75382d401aa023a496072c12af164e2a97e
DanilaKorsakov <109290153+DanilaKorsakov@users.noreply.github.com>
2024-11-07 15:58:53 +0300

    add field Flat.owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -56,7 +56,7 @@ class Flat(models.Model):
         related_name='liked_flats',
         verbose_name='Кто лайкнул'
     )
-    owner_pure_phone = PhoneNumberField(blank=True, null=True)
+    owner_pure_phone = PhoneNumberField(blank=True, null=True, region="RU")
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
