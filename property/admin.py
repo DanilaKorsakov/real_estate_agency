da0fb3dc485ebc2a08d358fe5d357d5c8c8f221b
BasicTemplates <kvashnin.danil@yandex.ru>
2025-02-13 15:09:39 +0300

    showed flats owners

--- property/admin.py
+++ property/admin.py
@@ -2,23 +2,32 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner', )
+
+
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address',  'owner']
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber','owner_pure_phone' )
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     list_editable = ['new_building']
     raw_id_fields=['liked_by']
+    inlines = [
+        OwnerInline,
+    ]
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('author', 'flat')
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     search_fields = ('name', 'pure_phone')
     raw_id_fields = ('flats',)
 
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
+
--- property/models.py
+++ property/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -56,7 +54,6 @@ class Flat(models.Model):
         related_name='liked_flats',
         verbose_name='Кто лайкнул'
     )
-    owner_pure_phone = PhoneNumberField(blank=True, null=True, verbose_name ='Нормализованный номер владельца')
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
